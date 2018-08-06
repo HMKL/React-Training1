8bc32eb9fa21eb49f2b6fe39e8fd8552429f69c4
HMKL <catalin.bac@gmail.com>
2018-08-06 21:12:27 +0300

    added eslint, fixed errors

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react';
-import MyHeader from './components/Header'; 
+import MyHeader from './components/Header';
 import ColorChooser from './components/ColorChooser';
 import './App.css';
 
 
 class App extends Component {
   state = {
-    selectedColor: ""
+    selectedColor: '',
   }
 
   onColorChange = (event) => {
     this.setState({
-      selectedColor: event.target.value
-    })
+      selectedColor: event.target.value,
+    });
   }
 
   render() {
     return (
       <div className="App">
-      <MyHeader 
-      title={"Hello React-Demo" }
-      color={this.state.selectedColor}
-      />
-      <div>
-        <ColorChooser 
-        selectedColor={this.state.selectedColor}
-        onColorChange={this.onColorChange}
+        <MyHeader
+          title="Hello React-Demo"
+          color={this.state.selectedColor}
         />
-      </div>
+        <div>
+          <ColorChooser
+            selectedColor={this.state.selectedColor}
+            onColorChange={this.onColorChange}
+          />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
-const MyHeader = (props) => {
-        //const { title } = props;
-        return(
-            <header>
-                <h1>{props.title}</h1>
-                <h3>Your favorite color is: {props.color}</h3>
-            </header>
-        );
-    }
-
-export default MyHeader
+const MyHeader = props => (
+  <header>
+    <h1>
+      {props.title}
+    </h1>
+    <h3>
+        Your favorite color is:
+      
+      {props.color}
+    </h3>
+  </header>
+);
+export default MyHeader;
